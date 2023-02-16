0d43a2dd806c51e2d9f806cc111391abca3f2475
ShindeyM <shindeymadrid@gmail.com>
2023-02-15 22:00:58 -0300

    endpoint home

--- controllers/homeService.js
+++ controllers/homeService.js
@@ -1,1 +1,12 @@
 const knex = require("../config/knexfile");
+
+// MOSTRAR TODOS LOS INMUEBLES EN HOME
+exports.home = (req, res) => {
+  knex("inmuebles")
+    .then((resultado) => {
+      res.json(resultado);
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error.message });
+    });
+};
